13450854ac222c6c1d49773d5e209bf7addcb03c
saiprakash talakanti <talakantisaiprakash@gmail.com>
2023-01-11 22:46:12 +0530

    created

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,56 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>India National Cricket Team</h1>
-<div>
-  <h2>BATSMEN</h2>
-<div className="player">
-<div><img src="./indianplayers/virat-kohli.webp" alt="Virat Kohli"></img><h4>Virat Kohli</h4></div>
-<div><img src="./indianplayers/rohit-sharma.webp" alt="Rohit Sharma"></img><h4>Rohit Sharma</h4></div>
-<div><img src="./indianplayers/shikhar-dhawan.webp" alt="Shikhar Dhawan"></img><h4>Shikhar Dhawan</h4></div>
-<div><img src="./indianplayers/shreyas-iyer.webp" alt="shreyas iyer"></img><h4>Shreyas Iyer</h4></div>
-<div><img src="./indianplayers/mayank-agarwal.webp" alt="mayank-agarwal"></img><h4>Mayank Agarwal</h4></div>
-
-
-</div>
-</div>
-<div>
-  <h2>ALL ROUNDER </h2>
-  <div className="player">
-<div><img src="./indianplayers/hardik-pandya.webp" alt="hardik-pandya"></img><h4>Hardik Pandya</h4></div>
-<div><img src="./indianplayers/ravichandran-ashwin.webp" alt="ravichandran-ashwin"></img><h4>Ravichandran Ashwin</h4></div>
-<div><img src="./indianplayers/ravindra-jadeja.webp" alt="ravindra-jadeja"></img><h4>Ravindra-Jadeja</h4></div>
-
-
-
-</div>
-  </div>
-<div>
-  <h2>WICKET KEEPER </h2>
-  <div className="player">
-<div><img src="./indianplayers/kl-rahul.webp" alt="K L Rahul"></img><h4>K L Rahul</h4></div>
-<div><img src="./indianplayers/rishabh-pant.webp" alt="rishabh-pant"></img><h4>Rishabh Pant</h4></div>
-<div><img src="./indianplayers/sanju-samson.webp" alt="sanju-samson"></img><h4>Sanju Samson</h4></div>
-
-
-</div>
-  </div>
-<div>
-  <h2>BOWLER</h2>
-  <div className="player">
-<div><img src="./indianplayers/kuldeep-yadav.webp" alt="kuldeep-yadav"></img><h4>Kuldeep Yadav</h4></div>
-<div><img src="./indianplayers/yuzvendra-chahal.webp" alt="yuzvendra-chahal"></img><h4>Yuzvendra Chahal</h4></div>
-<div><img src="./indianplayers/mohammed-shami.webp" alt="mohammed-shami"></img><h4>Mohammed Shami</h4></div>
-<div><img src="./indianplayers/mohammed-siraj.webp" alt="mohammed-siraj"></img><h4>Mohammed Siraj</h4></div>
-<div><img src="./indianplayers/jasprit-bumrah.webp" alt="jasprit-bumrah"></img><h4>Jasprit Bumrah</h4></div>
-
-
-</div>
-  </div>
-    <h4>Radha Rajput</h4>
+      <div>
+        <h2>BATSMEN</h2>
+        <div className="player">
+          <div>
+            <img src="./indianplayers\virat-kohli.webp" alt="Virat Kohli"></img>
+            <h4>Virat Kohli</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/rohit-sharma.webp"
+              alt="Rohit Sharma"
+            ></img>
+            <h4>Rohit Sharma</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/shikhar-dhawan.webp"
+              alt="Shikhar Dhawan"
+            ></img>
+            <h4>Shikhar Dhawan</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/shreyas-iyer.webp"
+              alt="shreyas iyer"
+            ></img>
+            <h4>Shreyas Iyer</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/mayank-agarwal.webp"
+              alt="mayank-agarwal"
+            ></img>
+            <h4>Mayank Agarwal</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>ALL ROUNDER </h2>
+        <div className="player">
+          <div>
+            <img
+              src="./indianplayers\hardik-pandya.webp"
+              alt="hardik-pandya"
+            ></img>
+            <h4>Hardik Pandya</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/ravichandran-ashwin.webp"
+              alt="ravichandran-ashwin"
+            ></img>
+            <h4>Ravichandran Ashwin</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/ravindra-jadeja.webp"
+              alt="ravindra-jadeja"
+            ></img>
+            <h4>Ravindra-Jadeja</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>WICKET KEEPER </h2>
+        <div className="player">
+          <div>
+            <img src="./indianplayers/kl-rahul.webp" alt="K L Rahul"></img>
+            <h4>K L Rahul</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/rishabh-pant.webp"
+              alt="rishabh-pant"
+            ></img>
+            <h4>Rishabh Pant</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/sanju-samson.webp"
+              alt="sanju-samson"
+            ></img>
+            <h4>Sanju Samson</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>BOWLER</h2>
+        <div className="player">
+          <div>
+            <img
+              src="./indianplayers/kuldeep-yadav.webp"
+              alt="kuldeep-yadav"
+            ></img>
+            <h4>Kuldeep Yadav</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/yuzvendra-chahal.webp"
+              alt="yuzvendra-chahal"
+            ></img>
+            <h4>Yuzvendra Chahal</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/mohammed-shami.webp"
+              alt="mohammed-shami"
+            ></img>
+            <h4>Mohammed Shami</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/mohammed-siraj.webp"
+              alt="mohammed-siraj"
+            ></img>
+            <h4>Mohammed Siraj</h4>
+          </div>
+          <div>
+            <img
+              src="./indianplayers/jasprit-bumrah.webp"
+              alt="jasprit-bumrah"
+            ></img>
+            <h4>Jasprit Bumrah</h4>
+          </div>
+        </div>
+      </div>
+      <h4>Radha Rajput</h4>
     </div>
   );
 }
